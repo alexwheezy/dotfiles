790bfa9e82afd707fdc76a487c480816dff51e81
Alexander Alekhin <sphrand@gmail.com>
2024-07-06 05:35:38 +0300

    add langmapper plugin

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -105,3 +105,23 @@ opt.termguicolors = true
 
 -- When there is a previous search pattern, highlight all its matches.
 opt.hlsearch = true
+
+local function escape(str)
+  -- You need to escape these characters to work correctly
+  local escape_chars = [[;,."|\]]
+  return vim.fn.escape(str, escape_chars)
+end
+
+-- Recommended to use lua template string
+local en = [[`qwertyuiop[]asdfghjkl;'zxcvbnm]]
+local ru = [[ёйцукенгшщзхъфывапролджэячсмить]]
+local en_shift = [[~QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>]]
+local ru_shift = [[ËЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ]]
+
+vim.opt.langmap = vim.fn.join({
+  -- | `to` should be first     | `from` should be second
+  escape(ru_shift)
+    .. ";"
+    .. escape(en_shift),
+  escape(ru) .. ";" .. escape(en),
+}, ",")
