0b55fae5a3504136f28e2658e5ed973109d59f58
Alexander Alekhin <sphrand@gmail.com>
2025-05-26 17:48:24 +0300

    Update the default config

--- .config/wm/dwm/config.h
+++ .config/wm/dwm/config.h
@@ -9,11 +9,13 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "SauceCodeProNerdFont:size=14" };
 static const char dmenufont[]       = { "SauceCodeProNerdFont:size=14" };
+static const char col_base[]        = "#13131e";
 static const char col_blue[]        = "#47698f";
+static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_base[]        = "#13131e";
+static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_base,  col_gray2 },
