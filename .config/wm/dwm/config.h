d149063ac5b8fa39dc11977a1a8e7c6c48717d9e
Alexander Alekhin <sphrand@gmail.com>
2025-08-07 10:29:22 +0300

    remove setlaout{0}

--- .config/wm/dwm/config.h
+++ .config/wm/dwm/config.h
@@ -114,7 +114,6 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ 0,                            XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
